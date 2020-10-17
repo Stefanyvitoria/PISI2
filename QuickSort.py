91448e53ddcce5c24dbfc9ff8ae25db0c8dbb48e
Stefany Izidio <Stefanyvitoria9307@gmail.com>
2020-10-16 23:38:50 -0300

    corrigindo o quicksort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,46 +1,48 @@
-def quicksort(alist, first, last):
+import sys, random
+sys.setrecursionlimit(1000000)
+
+def quick_sort(alist, first, last):
     """Odena um vetor em ordem crescente."""
     if first < last:
         q = partition(alist, first, last)
-        quicksort(alist, first, q-1)
-        quicksort(alist, q+1, last)
+        quick_sort(alist, first, q)
+        quick_sort(alist, q+1, last)
 
 def partition(alist,f,l):
-    x = alist[l] #pivô
-    i = f-1
-    
-    for j in range(f,l):
-        if alist[j] <= x:
-            i = i+1
-            alist[i], alist[j] = alist[j], alist[i]
-    alist[i+1],alist[l]  = alist[l], alist[i+1]
-    return i+1
+    x = alist[l-1] #pivô
+
+    for i in range(f,l):
+        if alist[i] <= x:
+            f += 1
+            l += 1
+            alist[i], alist[f-1] = alist[f-1], alist[i]
+        else:
+            l += 1
+    return f-1
 
 
-comp = 0
-troca = 0
-def quicksort_registros(alist, first, last):
-    """Odena um vetor em ordem crescente e retorna o número de comparações e trocas."""
+comp = troca = 0
+def R_quick_sort(alist, first, last):
     global comp, troca
-    
+    """Odena um vetor em ordem crescente."""
     if first < last:
-        q = partition_registros(alist, first, last)
-        quicksort_registros(alist, first, q-1)
-        quicksort_registros(alist, q+1, last)
-    return [comp, troca]
+        q = R_partition(alist, first, last)
+        R_quick_sort(alist, first, q)
+        R_quick_sort(alist, q+1, last)
+        return [comp, troca]
 
-def partition_registros(alist,f,l):
+
+def R_partition(alist,f,l):
     global comp, troca
-    
-    x = alist[l] #pivô
-    i = f-1
-    for j in range(f,l):
+    x = alist[l-1] #pivô
+
+    for i in range(f,l):
         comp += 1
-        if alist[j] <= x:
+        if alist[i] <= x:
+            f += 1
+            l += 1
             troca += 1
-            i = i+1
-            alist[i], alist[j] = alist[j], alist[i]
-            
-    troca += 1
-    alist[i+1],alist[l]  = alist[l], alist[i+1]
-    return i+1
+            alist[i], alist[f-1] = alist[f-1], alist[i]
+        else:
+            l += 1
+    return f-1
