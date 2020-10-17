ec5f35f756fdb623fd8fb678b955e3da000363d0
Stefany Izidio <Stefanyvitoria9307@gmail.com>
2020-10-17 11:56:14 -0300

    Adicionando testes para verificar a ordenação

---
+++ teste.py
@@ -0,0 +1,9 @@
+import random
+
+dados = [random.randint(0,20) for _ in range(100)]
+dados = [ele for ele in range(100)]
+dados = [ele for ele in range(0,100,-1)]
+
+a = dados.copy()
+b = dados.copy()
+
