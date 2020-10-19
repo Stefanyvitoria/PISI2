98cf66c2163a821e45d9d203593ca04db533fcdb
Stefany Izidio <Stefanyvitoria9307@gmail.com>
2020-10-19 10:12:05 -0300

    melhorando CriandoDB.py

--- CriandoDB.py
+++ CriandoDB.py
@@ -1,73 +1,60 @@
 import shelve, random
 
 def CreateDBAleatoria():
-    """gera um data base nomeado DBAleatoria com 300 vetores com 500, 1000 e 10.000 elementos aleatórios entre 0 e 100.000"""
+    """Gera um Data Base nomeado DBAleatoria com 50 vetores com 500 elementos,
+    30 com 1000 e 3 com 10.000, todos em ordem aleatória e entre 0 e 10.000 elementos."""
     
     dataBase = shelve.open('DBAleatoria')
+    
     for num in range(1,50+1): 
-        dt = []
-        while len(dt) < 500:
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = dt
+        vetor = [random.randint(0,10000) for _ in range(500)]
+        dataBase[str(num)] = vetor
 
     for num in range(51,80+1): 
-        dt = []
-        while len(dt) < 1000: 
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = dt
+        vetor = [random.randint(0,10000) for _ in range(1000)]
+        dataBase[str(num)] = vetor
 
     for num in range(81,83+1):
-        dt = []
-        while len(dt) < 10000: 
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = dt
+        vetor = [random.randint(0,10000) for _ in range(10000)] 
+        dataBase[str(num)] = vetor
+    
     dataBase.close()
 
 def CreateDBOrdenada():
-    """gera um data base nomeado DBOrdenada com 300 vetores com 500, 1000 e 10.000 elementos ordenados crescentemente entre 0 e 100.000"""
+    """Gera um Data Base nomeado DBOrdenada com 50 vetores com 500 elementos,
+    30 com 1000 e 3 com 10.000 elementos, todos ordenados crescentemente e entre 0 e 10.000 elementos."""
 
     dataBase = shelve.open('DBOrdenada')
+
+    vetor = [ele for ele in range(1,501)]
     for num in range(1,50+1): 
-        dt = []
-        while len(dt) < 500: 
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = sorted(dt)
+        dataBase[str(num)] = vetor
 
-    for num in range(51,80+1): 
-        dt = []
-        while len(dt) < 1000: 
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = sorted(dt)
+    vetor = [ele for ele in range(1,1001)]
+    for num in range(51,80+1):  
+        dataBase[str(num)] = vetor
 
-    for num in range(81,83+1): 
-        dt = []
-        while len(dt) < 10000: 
-            dt.append(random.randint(0,100000)) 
-        dataBase[str(num)] = sorted(dt)
+    vetor = [ele for ele in range(1,10001)]
+    for num in range(81,83+1):  
+        dataBase[str(num)] = vetor
+
     dataBase.close()  
 
 def CreateDBInversa():
-    """gera um data base nomeado DBInversa com 300 vetores com 500, 1000 e 10.000 elementos Ordenados em ordem decrescente entre 0 e 100.000"""
+    """gera um data base nomeado DBInversa com 300 vetores com 500, 1000 e 10.000 elementos Ordenados em ordem decrescente entre 0 e 10.000"""
     
     dataBase = shelve.open('DBInversa')
+
+    vetor = [ele for ele in range(500,0,-1)]
     for num in range(1,50+1): 
-        dt = []
-        while len(dt) < 500: 
-            dt.append(random.randint(0,100000))
-        dt = [ele for ele in reversed(sorted(dt))]
-        dataBase[str(num)] = dt
+        dataBase[str(num)] = vetor
 
+    vetor = [ele for ele in range(1000,0,-1)]
     for num in range(51,80+1): 
-        dt = []
-        while len(dt) < 1000: 
-            dt.append(random.randint(0,100000)) 
-        dt = [ele for ele in reversed(sorted(dt))]
-        dataBase[str(num)] = dt
+        dataBase[str(num)] = vetor
 
+    vetor = [ele for ele in range(10000,0,-1)]
     for num in range(81,83+1): 
-        dt = []
-        while len(dt) < 10000: 
-            dt.append(random.randint(0,100000)) 
-        dt = [ele for ele in reversed(sorted(dt))]
-        dataBase[str(num)] = dt
+        dataBase[str(num)] = vetor
+
     dataBase.close()
